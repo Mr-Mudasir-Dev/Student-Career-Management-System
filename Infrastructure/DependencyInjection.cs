8eea6fc22b26ed8a3d3d0079af2ec285bd6780b9
ahmedrehmancode <ahmedrehman0010@gmail.com>
2026-06-25 16:22:48 +0500

    Applying Identity On Project

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Infrastructure.Data;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -14,6 +18,25 @@ namespace Infrastructure
         this IServiceCollection services,
         IConfiguration configuration)
         {
+
+            //Database 
+            services.AddDbContext<AppDbContext>(opt =>
+            {
+                opt.UseSqlServer(configuration.GetConnectionString("DbConnection"));
+            });
+
+            //Identiy
+            services.AddIdentity<ApplicationUser, IdentityRole>(option =>
+            {
+                option.Password.RequiredLength = 6;
+                option.Password.RequireDigit = true;
+                option.Password.RequireUppercase = true;
+                option.Password.GetHashCode();
+
+            })
+             .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+
             return services;
         }
     }
