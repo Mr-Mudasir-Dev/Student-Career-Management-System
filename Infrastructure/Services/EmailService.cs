c83c7e72071d396841ecc528be57620d2247f4e8
ahmedrehmancode <ahmedrehman0010@gmail.com>
2026-07-28 15:48:04 +0500

    Email verification Almost Done

--- Infrastructure/Services/EmailService.cs
+++ Infrastructure/Services/EmailService.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Domain.Exceptions;
 
 namespace Infrastructure.Services
 {
@@ -27,6 +28,7 @@ namespace Infrastructure.Services
         public async Task<bool> SendVerificationEmailAsync(string email, string verificationLink)
         {
             try {
+                _logger.LogInformation($"Email Sender : Sending verification email to {email}...");
                 var message = new MimeMessage();
 
                 message.From.Add(new MailboxAddress(_emailConfig.SenderName, _emailConfig.SenderEmail));
@@ -40,9 +42,11 @@ namespace Infrastructure.Services
                     Text = $"<p>Click the link below to verify your email:</p><a href='{verificationLink}'>Verify Email</a>"
                 };
 
+                _logger.LogInformation($"Email Sender : Connecting to SMTP server {_emailConfig.SmtpServer}:{_emailConfig.Port}...");
+
                 using var client = new SmtpClient();
 
-                await client.ConnectAsync(_emailConfig.SmtpServer, _emailConfig.Port, false);
+                await client.ConnectAsync(_emailConfig.SmtpServer, _emailConfig.Port, SecureSocketOptions.StartTls);
 
                 await client.AuthenticateAsync(_emailConfig.Username, _emailConfig.Password);
 
@@ -59,7 +63,7 @@ namespace Infrastructure.Services
             {
 
                 _logger.LogError($"Something Wen't Wrong : {ex.Message}");
-                throw new Exception($"Something Wen't Wrong: {ex.Message}");
+                throw new AppException("Something Wen't Wrong");
  
             }
         }
